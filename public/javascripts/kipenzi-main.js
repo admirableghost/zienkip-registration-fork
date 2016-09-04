cfdc2212391ffa69961c7f6df91a2a11162ceed4
Jose Tom <josetom92@gmail.com>
2016-09-04 16:26:29 +0530

    refactoring main.js

--- public/javascripts/kipenzi-main.js
+++ public/javascripts/kipenzi-main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app = angular.module('kipenzi-bro', ['kipenzi-router', 'kipenzi-login', 'kipenzi-dashboard',
-                                         'menu-home', 'menu-appointment', 'menu-appointment_history', 'menu-calendar', 'menu-inventory', 'menu-profile_management']);
+                                         'menu-home', 'menu-appointment-register', 'menu-appointment-history', 'menu-calendar', 'menu-inventory', 'menu-profile_management']);
 
 app.service('userService', function(kipenziFactory) {        
     
@@ -81,105 +81,3 @@ app.config(['$httpProvider','$authProvider', function($httpProvider,$authProvide
 //            }
 //        });
     }]);
-
-
-
-
-// ----------------
-
-
-var localcustomer = {
-    firstname      : "First",
-    lastname       : "Last",
-    mobilenumber   : "9655507966",
-    gender         : "M",
-    petInfo        : [
-        {
-            "name"      : "Tommy",
-            "gender"    : "M",
-            "type"      : "Dog",
-            "breed"     : "Pug",
-            "age"       : "4weeks",
-            "history"   : [{"sickness" : "Fever", "treatment" : "Vaccine", "appointmentDate" : "12Jan2016"},
-                           {"sickness" : "Cold", "treatment" : "Vaccine", "appointmentDate" : "12Mar2016"}]
-        },
-        {
-            "name"      : "Giggles",
-            "gender"    : "M",
-            "type"      : "Dog",
-            "breed"     : "Pug",
-            "age"       : "4weeks",
-            "history"   : [{"sickness" : "Fever", "treatment" : "Vaccine", "appointmentDate" : "12Jan2016"},
-                           {"sickness" : "Cold", "treatment" : "Vaccine", "appointmentDate" : "12May2016"}]
-        },
-        {
-            "name"      : "Hamster",
-            "gender"    : "M",
-            "type"      : "Dog",
-            "breed"     : "Pug",
-            "age"       : "4weeks",
-            "history"   : [{"sickness" : "Fever", "treatment" : "Vaccine", "appointmentDate" : "12Jan2016"},
-                           {"sickness" : "Cold", "treatment" : "Vaccine", "appointmentDate" : "12Jun2016"}]
-        }    
-    ]
-};
-
-
-
-// List of  Customer
-app.service('customer_list_service', function() { 
-   this.customerList = new ArrayList();
-   //this.customerList.push(localcustomer);
-   this.customerList.add(localcustomer);
-});
-
-// Menu - Home Screen (or) Dashboard
-app.controller('home_controller', function($scope, $rootScope, $http, $state) {   
-    $rootScope.bodyClass = "nav-md";
-});
-
-// Menu - Appointment History
-app.controller('appointment_history_controller', function($scope, $http, $state) {
-    
-});
-
-// Menu - Appointments
-app.controller('appointment_controller', function($scope, $http, $state) {
-    $scope.profile_management_controller_firstName = "Dr. Irfan";
-    $scope.profile_management_controller_lastName = "Khan";
-    $scope.profile_management_controller_mobileNumber = "Farhan";
-    $scope.profile_management_controller_eMail = "irfan@gmail.com";
-    $scope.profile_management_controller_gender = "male";
-});
-
-// Menu - Calendar
-app.controller('calendar_controller', function($scope, $http, $state) {
-   
-});
-
-// Menu - Profile Managenment
-app.controller('profile_management_controller', function($scope, $http, $state) {
-    $scope.profile_management_controller_FirstName = "Dr. Irfan";
-    $scope.profile_management_controller_LastName = "Khan";
-    $scope.profile_management_controller_MobileNumber = "Farhan";
-    $scope.profile_management_controller_EMail = "irfan@gmail.com";
-    $scope.profile_management_controller_Gender = "male";
-    $scope.profile_management_controller_DOB_Date = "2";
-    $scope.profile_management_controller_DOB_Month = "May";
-    $scope.profile_management_controller_DOB_Year = "1990";
-    $scope.profile_management_controller_ClinicName = "AK Vet CLinic";
-    $scope.profile_management_controller_Clinic_FromTime = "10am";
-    $scope.profile_management_controller_Clinic_ToTime = "7pm";
-    $scope.profile_management_controller_AvgConsultDuration = "30 mins";
-    $scope.profile_management_controller_ConsultationFees = "150";
-    $scope.profile_management_controller_Experience_Value = 2;
-    $scope.profile_management_controller_Experience_Duration = "years";
-    $scope.profile_management_controller_Specialization = "Dogs";
-    $scope.profile_management_controller_Qualifications = "MD";
-    $scope.profile_management_controller_SelfDescription = "Professional with established reputation";
-});
-
-// Menu - Inventory Management
-app.controller('inventory_controller', function($scope, $http, $state) {
-   
-});
